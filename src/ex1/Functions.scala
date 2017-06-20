2eeca444132e791123c06015b7bf803bc89407a4
Svetlozar Kondakov <skondakoff@gmail.com>
2017-06-20 23:23:58 +0300

    Solved first 4 examples

--- src/ex1/Functions.scala
+++ src/ex1/Functions.scala
@@ -3,17 +3,35 @@ package ex1
 object Functions {
 
   // Връща големината на масив (без да ползва data.length!!!)
-  def length(data: List[Int]) = ??? 
-
+  def length(data: List[Int]): Int = if (data.isEmpty) 0 else length(data.tail) + 1
+  
   // Ако cond е true връща onTrue
-  def ifelse(cond: Boolean, onTrue: Int, onFalse: Int) = ???
+  def ifelse(cond: Boolean, onTrue: => Int, onFalse: => Int) = if (cond) onTrue else onFalse
 
   // Проверява дали скобите в даден масив от символи са балансирани.
   // Коректно: (a)asda(b)(v) | (((a))) | ()(()асдасд)
   // Грешно: )() | ((д) | ((das) (d)( 
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char], open: Int = 0): Boolean = {
+    
+    if (chars.isEmpty) return open == 0
+      
+    var newOpen = open;
+    if (chars.head == ')') {
+      if (open <= 0)
+        return false
+      else 
+        newOpen = open - 1
+    } else if (chars.head == '(') {
+       newOpen = open + 1;
+    }
 
-  def map(chars: List[Char], f: Any) =  ???
+    return balance(chars.tail, newOpen)
+  }
+
+  def map(chars: List[Char], f: Char => Unit): Unit = {
+      f(chars.head)
+      if (!chars.tail.isEmpty) map(chars.tail, f)
+  }
 
   def toUpperCase(chars: List[Char]) = {
     def upperCase(char: Char) = ???
